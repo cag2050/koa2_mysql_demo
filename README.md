d49cbd20a2ee8eba41bb0875e903834db51b8695
cag2050 <cag2050@163.com>
2017-04-27 15:20:28 +0800

    m

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
 ## 启动方式
-
 最简单启动方式
-
 ```
 node bin/run
 ```
-
 最常用的集成nodemon的方式,代码变动会自动重载(其实就是nodemon去执行bin/run)
-
 ```
 npm start
 ```
-
 支持pm2部署
-
 ```
  #npm run pm2
  pm2 start bin/run 
 ```
+## 访问地址
+http://localhost:3000/
---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {
+    port: 3001,
+    database: {
+        DATABASE: 'member',
+        USERNAME: 'root',
+        PASSWORD: 'mysql',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
